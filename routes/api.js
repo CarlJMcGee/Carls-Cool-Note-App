184499f644d5194dac6225e7050cc8f1e5f7fe82
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-05-09 19:00:16 -0500

    fixed note.html not loading index.js

---
+++ routes/api.js
@@ -0,0 +1,59 @@
+const express = require("express");
+const router = express.Router();
+const saveNotes = require("../util/writeFile.js");
+let notes = require("../db/db.json");
+
+const filterByQuery = (query, arr) => {
+  let titleArr = [];
+  let filtered = arr;
+
+  if (query.title) {
+    if (typeof query.title === "string") {
+      titleArr = [query.title];
+    } else {
+      titleArr = query.title;
+    }
+    titleArr.forEach((title) => {
+      filtered = filtered.filter((note) => note.title.indexOf(title) !== -1);
+    });
+  }
+
+  if (query.title) {
+    filtered = filtered.filter((note) => note.title === query.title);
+  }
+  if (query.text) {
+    filtered = filtered.filter((note) => note.text === query.text);
+  }
+  return filtered;
+};
+
+const findById = (id, arr) => {
+  const result = arr.filter((note) => note.id === id)[0];
+  return result;
+};
+
+router.get("/notes", (req, res) => {
+  res.header("Content-Type", "application/json");
+  let results = notes;
+  if (req.query) {
+    results = filterByQuery(req.query, results);
+  }
+  res.json(results);
+});
+
+router.get("/notes/:id", (req, res) => {
+  let result = findById(req.params.id, notes);
+
+  res.json(result);
+});
+
+router.post("/notes", (req, res) => {
+  const isValid = true;
+  if (isValid) {
+    console.log("Note sent!");
+    saveNotes(req.body);
+  }
+  res.json(req.body);
+});
+
+module.exports = router;
--- server.js
+++ server.js
@@ -2,59 +2,21 @@ const express = require("express");
 const path = require("path");
 const PORT = process.env.PORT || 3001;
 const app = express();
-const { notes } = require("./db/db.json");
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static("public"));
 
-app.listen(PORT, () => {
-  console.log("API server now on port " + PORT);
-});
+const apiRouter = require("./routes/api.js");
 
-const filterByQuery = (query, arr) => {
-  let titleArr = [];
-  let filtered = arr;
-
-  if (query.title) {
-    if (typeof query.title === "string") {
-      titleArr = [query.title];
-    } else {
-      titleArr = query.title;
-    }
-    titleArr.forEach((title) => {
-      filtered = filtered.filter((note) => note.title.indexOf(title) !== -1);
-    });
-  }
-
-  if (query.title) {
-    filtered = filtered.filter((note) => note.title === query.title);
-  }
-  if (query.text) {
-    filtered = filtered.filter((note) => note.text === query.text);
-  }
-  return filtered;
-};
-
-const findById = (id, arr) => {
-  const result = arr.filter((note) => note.id === id)[0];
-  return result;
-};
+app.use("/api", apiRouter);
 
 app.get("/", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/index.html"));
 });
-
 app.get("/notes", (req, res) => {
   res.sendFile(path.join(__dirname, "/public/notes.html"));
 });
 
-app.get("/api/notes", (req, res) => {
-  let results = notes;
-  if (req.query) {
-    results = filterByQuery(req.query, results);
-  }
-  res.json(results);
-});
-
-app.get("/api/test/:id", (req, res) => {
-  let result = findById(req.params.id, notes);
-
-  res.json(result);
+app.listen(PORT, () => {
+  console.log("API server now on port " + PORT);
 });
