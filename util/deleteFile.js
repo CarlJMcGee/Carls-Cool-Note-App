4152230175650612bb1ba73f0c3e6e8190029061
CarlJMcGee <98133059+CarlJMcGee@users.noreply.github.com>
2022-05-11 10:07:03 -0500

    made deleteFile sync

--- util/deleteFile.js
+++ util/deleteFile.js
@@ -2,9 +2,9 @@ const fs = require("fs");
 const fsProm = require("fs/promises");
 let notes = require("../db/db.json");
 
-const deleteFile = async function (id) {
+const deleteFile = function (id) {
   keptNotes = notes.filter((note) => note.id !== id);
-  await fsProm.writeFile("./db/db.json", JSON.stringify(keptNotes));
+  fs.writeFileSync("./db/db.json", JSON.stringify(keptNotes));
   console.log("Note Deleted");
 };
 
